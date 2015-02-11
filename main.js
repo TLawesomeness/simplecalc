af3d936295055b8694d6564b43fbb6c795eda5f8
Tommyluo <tluo12321@yahoo.com>
2015-02-11 14:25:27 -0800

    almost done, missing exponent

--- main.js
+++ main.js
@@ -14,10 +14,29 @@ function init() {
      $('.screen').text("");
    })
 
-   $('.keys').on('click', '.eval', function(){
+   $('.keys').on('click', '.eval', function() {
      var answer = eval($('.screen').text());
      $('.screen').text(answer);
    })
 
-   
+   $('.keys').on('click', '.flipsign', function () {
+     var answer = eval($('.screen').text());
+     var flipsign = answer * -1;
+     $('.screen').text(flipsign);
+   })
+
+   $('.keys').on('click', '.sqrt', function() {
+     var answer = eval($('.screen').text());
+     var sqrt = Math.sqrt(answer);
+     $('.screen').text(sqrt);
+   })
+
+  //  $('.keys').on('click', '.exponent', function() {
+  //    var answer = eval($('.screen').text());
+  //    var exponent = Math.pow(answer);
+  //    $('.screen').text(exponent);
+  //  })
+
+
+
 }
